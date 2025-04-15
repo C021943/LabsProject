ec2f47c6fdf3b74e8b3003d69698432541365a2c
C021943 <147182983+C021943@users.noreply.github.com>
2025-04-15 06:15:21 -0300

    Add files via upload

---
+++ 02_bronze/purchasesprices_bronze.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("Ingesta Bronze Purchases") \
+    .config("spark.jars.packages", "io.delta:delta-core_2.12:2.4.0") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .getOrCreate()
+
+INPUT_FOLDER = "./datalake/prelanding/PurchasePrices"
+OUTPUT_FOLDER = "./datalake/bronze/PurchasePrices"
+#basicamente tomamos la info de la prelanding cruda la convertimos en delta y guardamos en la zona bronze
+
+try:
+    df = spark.read.option("header", True).csv(INPUT_FOLDER)
+    df.write.format("delta").mode("overwrite").save(OUTPUT_FOLDER)
+except Exception as e:
+    raise e
