f1006d44a7bf827ee22b087a206637dc491f5d96
C021943 <147182983+C021943@users.noreply.github.com>
2025-04-15 15:06:38 -0300

    Create marginsprofitsbrand_postgres.py

---
+++ notebooks/05_postgres/marginsprofitsbrand_postgres.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder \
+    .appName("Exportar a PostgreSQL") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+    .getOrCreate()
+
+# levantamos la tabla delta de gold
+delta_table_path = "./datalake/gold/MarginsProfitsBrand"
+df = spark.read.format("delta").load(delta_table_path)
+
+# display para verificar lo que enviamos
+df.show()
+
+#configuración
+jdbc_url = "jdbc:postgresql://shuttle.proxy.rlwy.net:19123/railway"
+properties = {
+    "user": "postgres",
+    "password": "ItoEzlEowjgdjQaieWCSWakiBjmvsJmB",
+    "driver": "org.postgresql.Driver"
+}
+
+#escritura en postgres
+df.write \
+    .jdbc(
+        url=jdbc_url,
+        table="margins_profits_brand",
+        mode="overwrite",  
+        properties=properties
+    )
