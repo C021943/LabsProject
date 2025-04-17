cbf9c0a0d34cd98c9b8ec7625ec670175c83a73f
C021943 <147182983+C021943@users.noreply.github.com>
2025-04-17 04:33:10 -0300

    Update beginv_bronze.py

--- notebooks/02_bronze/beginv_bronze.py
+++ notebooks/02_bronze/beginv_bronze.py
@@ -1,7 +1,7 @@
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder \
-    .appName("Ingesta Bronze Purchases") \
+    .appName("Ingestion Bronze Purchases") \
     .config("spark.jars.packages", "io.delta:delta-core_2.12:2.4.0") \
     .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
     .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
@@ -9,7 +9,7 @@ spark = SparkSession.builder \
 
 INPUT_FOLDER = "./datalake/prelanding/BegInv"
 OUTPUT_FOLDER = "./datalake/bronze/BegInv"
-#basicamente tomamos la info de la prelanding cruda la convertimos en delta y guardamos en la zona bronze
+#Basically, raw data from prelanding is taking and then converted and saved as Delta tables in Bronze layer
 try:
     df = spark.read.option("header", True).csv(INPUT_FOLDER)
     df.write.format("delta").mode("overwrite").save(OUTPUT_FOLDER)
